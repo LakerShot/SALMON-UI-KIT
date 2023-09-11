c7e6327d9bc819f235be0233a8c5f15f9dde6588
Morozov Egor <egormorozov4130@gmail.com>
2023-09-12 05:33:01 +0800

    chore: set up rollup config file

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import SalmonCdnIcon from "./components/SalmonCdnIcon";
+import SalmonButton from "./components/SalmonButton";
+import SalmonCircularProgress from "./components/SalmonCircularProgress";
+import SalmonTextField from "./components/SalmonTextField";
+
+export {
+    SalmonCdnIcon,
+    SalmonButton,
+    SalmonCircularProgress,
+    SalmonTextField
+}
